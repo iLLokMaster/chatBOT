f507eaf29d0c4137f393b1e3f96477976bc135ef
ifdark <157958263+ifdark@users.noreply.github.com>
2024-01-28 14:02:03 +0300

    Update1.0.1 main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ cursor = DataBaseSQL3inCode.cursor()
 #cursor.execute("INSERT INTO AnsvQest VALUES ('привет', 'Привет, как у тебя дела?')")
     ###select all from Data Base and insert to db
 db = cursor.execute("SELECT * FROM AnsvQest").fetchall()
+print(db)
     ###commit changes and cloce Data Base
 DataBaseSQL3inCode.commit()
 #DataBaseSQL3inCode.close()
@@ -20,24 +21,22 @@ def per_com(list1, list2): #calculation of the percentage of coincidence of the 
     common_elements = set(list1) & set(list2)
     percent_match = (len(common_elements) / len(set(list1 + list2))) * 100
     return percent_match
-
 def recp(prompt):
     pers = []
     pr = prompt
     pr = pr.lower()
     pr = re.sub('\W+',' ', pr )
-
     for k in range(len(db)):
         list_a = pr
         list_b = re.sub('\W+',' ', db[k][0] )
         result = per_com(list_a, list_b)
         pers.append([result, db[k][1]])
     sortPers = sorted(pers)
-
     #result = проценты совпадения
     if result == 0:
         print('я вас не понял')
     else:
+        #ответ бота
         print(sortPers[-1][1])
         if sortPers[-1][0] != 100:
             if input('вам понравился ответ?[да = anything][нет = .] ').lower() == '.':
@@ -45,13 +44,17 @@ def recp(prompt):
                 cursor.execute(f"INSERT INTO AnsvQest VALUES ('{prompt}', '{otvet_polzovatelya}')")
                 DataBaseSQL3inCode.commit()
                 db.append([prompt, otvet_polzovatelya])
-                print(db)
+                #print(db)
             #вам понравился ответ?
-
+            else:
+                cursor.execute(f"INSERT INTO AnsvQest VALUES ('{prompt}', '{sortPers[-1][1]}')")
+                DataBaseSQL3inCode.commit()
+                db.append([prompt, sortPers[-1][1]])
 while True:
     prompt = str(input("your prompt: "))
-    if prompt == 'exit':
+    if prompt == 'exit' or prompt == 'выход':
         DataBaseSQL3inCode.close()
         print('пока')
         exit()
     recp(prompt)
+
