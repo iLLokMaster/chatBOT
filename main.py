d832973bedb08165c1a11b6932b3f2a37f55a120
Роман <130349746+iLLokMaster@users.noreply.github.com>
2024-01-30 18:55:06 +0300

    Update main.py
    добавлены команды и рандом к ответам а них

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import sqlite3
 import os
 import random
 import time
-
+import webbrowser
+#connect Data Base
 DataBaseSQL3inCode = sqlite3.connect("DataBaseInFiles")
-    ###create cursor
+#create cursor
 cursor = DataBaseSQL3inCode.cursor()
+try:
+    cursor.execute("SELECT * FROM AnsvQest")
+except sqlite3.Error:
     ###create table
     ###perform only for the first time
-# cursor.execute("""CREATE TABLE AnsvQest (
-#     answer text,
-#     qestion text
-# )""")
-#cursor.execute("INSERT INTO AnsvQest VALUES ('привет', 'Привет, как у тебя дела?')")
-    ###select all from Data Base and insert to db
+    cursor.execute("""CREATE TABLE AnsvQest (
+            answer text,
+            qestion text
+        )""")
+    cursor.execute("INSERT INTO AnsvQest VALUES ('привет', 'Привет, как у тебя дела?')")
+###select all from Data Base and insert to db
 db = cursor.execute("SELECT * FROM AnsvQest").fetchall()
     ###commit changes and cloce Data Base
 DataBaseSQL3inCode.commit()
-#DataBaseSQL3inCode.close()
-    ###body of programm
+###body of programm
 def per_com(list1, list2): #calculation of the percentage of coincidence of the question from the database and the user's question
     common_elements = set(list1) & set(list2)
     percent_match = (len(common_elements) / len(set(list1 + list2))) * 100
@@ -65,17 +68,19 @@ def recp(prompt):
 while True:
     prompt = str(input("\nВаш вопрос: "))
     if prompt == 'exit' or prompt == 'выход':
-        print('сохраняем изменения в базе')
+        print('     сохраняем изменения в базе')
         try:
             DataBaseSQL3inCode.close()
-            print('изменения сохранены')
+            print('     изменения сохранены')
         except sqlite3.Error as e:
-            print("не удалось сохранить изменения")
+            print("     не удалось сохранить изменения")
             # get the error code
             error_code = e.args[0]
-            print(f"Error code: {error_code}")
-        print('пока')
-        print('Приведите пожалуйста поскорее. Я по вам уже скучаю:(')
+            print(f"     Error code: {error_code}")
+        if random.randint(1,2) == 1: #оптимизировать рандом. выбор
+            print('пока')
+        else:
+            print('Приходите пожалуйста поскорее. Я по вам уже скучаю:(')
         exit()
     elif prompt == 'db'.lower() or prompt == 'ви'.lower() or prompt == 'data base'.lower() or prompt == 'your data base'.lower() or prompt == 'твоя база данных'.lower():
         for h in range(len(db)):
@@ -85,6 +90,9 @@ while True:
     elif prompt == 'открой браузер':
         os.system('C:/Users/B-ZONE/AppData/Local/Yandex/YandexBrowser/Application/browser.exe')
         print('на')
+    elif prompt == 'какое дз':
+        webbrowser.open('https://dnevnik.ru/marks/school/1000009993521/student/1000012990748/current', new=2)
+        print('держи')
     elif prompt == 'открой сам пикер':
         os.system('C:/Program Files (x86)/Steam/Steam.exe')
         time.sleep(10)
